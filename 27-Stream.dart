516d3e2b7280ecd4095467ec49e0e132924d9331
TheLastGoddess <ela.farajzadeh1382@gmail.com>
2026-06-20 14:54:18 +0330

    Create 27-Stream.dart

---
+++ 27-Stream.dart
@@ -0,0 +1,9 @@
+import 'dart:async';
+
+void main(List<String> args) {
+  List<int> data = [1, 4, 5, 3, 67, 32];
+  Stream stream = Stream<int>.fromIterable(data);
+  stream.listen((value) {
+    print(value);
+  });
+}
