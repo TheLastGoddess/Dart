6c64658a5b81c0026f2513a33f10dbd0d64df510
TheLastGoddess <ela.farajzadeh1382@gmail.com>
2026-06-21 17:43:34 +0330

    Create 29-Stream.dart

---
+++ 29-Stream.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+import 'dart:math';
+
+void main(List<String> args) {
+  List<int> data = [1, 4, 5, 3, 67, 32];
+  Stream stream = Stream<int>.fromIterable(data);
+  Stream broadcast = stream.asBroadcastStream();
+  // broadcast.listen((value) => print(value));
+  broadcast.single
+      .then((value) => print("value is $value"))
+      .catchError((err) => print(err));
+  var SingleStream = stream.listen(null);
+  SingleStream.onData((value) => print(value));
+  SingleStream.onError((err) => print(err));
+  SingleStream.onDone(() => print("done"));
+}
