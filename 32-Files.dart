ac9d66a23afa7d6dea67b9a5cb3bd8e7541a0568
TheLastGoddess <ela.farajzadeh1382@gmail.com>
2026-06-23 15:42:13 +0330

    Update 32-Files.dart

--- 32-Files.dart
+++ 32-Files.dart
@@ -1,10 +1,14 @@
 import 'dart:io';
 
 Future main(List<String> args) async {
-  var config = File(r'C:\Users\Asus\Desktop\cli\bin\random.txt'); //add your text file's path
+  var config = File('random.txt');
   var contents;
-  contents = await config.readAsLines();
-  print(contents.length);
-  contents = await config.readAsString();
-  print(contents.length);
+  // contents = await config.readAsLines();
+  // print(contents.length);
+  try {
+    contents = await config.readAsString();
+    print(contents.length);
+  } catch (e) {
+    print("couldn't find the file");
+  }
 }
