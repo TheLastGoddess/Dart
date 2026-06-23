a414d8b90ad46af2e167cddecda849b16bd5a038
TheLastGoddess <ela.farajzadeh1382@gmail.com>
2026-06-23 16:25:26 +0330

    Create 34-files.dart

---
+++ 34-files.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+import 'dart:convert';
+
+Future main(List<String> args) async {
+  var config = File('new.txt');
+  var my_txt = config.openWrite();
+  my_txt.write('Hwllo miss Elaheh');
+  await my_txt.flush();
+  await my_txt.close();
+
+  config
+      .openRead()
+      .map(utf8.decode)
+      .transform(LineSplitter())
+      .forEach((l) => print(l));
+}
